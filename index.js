233d21295277c4203b0281d32bd83e37f3d6fb4e
AinaElie <elie.anjarafanomezana@gmail.com>
2024-08-08 18:20:11 +0300

    feat: instance en json

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import Patrimoine from "./models/Patrimoine.js";
 import Personne from "./models/Personne.js";
 import Flux from "./models/possessions/Flux.js";
 import Possession from "./models/possessions/Possession.js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const john = new Personne("John Doe");
 
 const macBookPro = new Possession(john, "MacBook Pro", 4000000, new Date("2023-12-25"), null, 5);
@@ -13,25 +16,27 @@ const possessions = [macBookPro,salaire,traindevie];
 
 const johnPatrimoine  = new Patrimoine(john,possessions);
 
-johnPatrimoine.addPossession(macBookPro);
-johnPatrimoine.addPossession(salaire);
-johnPatrimoine.addPossession(traindevie);
+// function save(personne, patrimoine) {
+//   const file = []
+//   file.push({
+//     model: "Personne",
+//     data: personne
+//   })
+//   file.push({
+//     model: "Patrimoine",
+//     data: patrimoine
+//   })
+//   return writeFile("./fileManager/data.json", file)
 
-function save(personne, patrimoine) {
-  const file = []
-  file.push({
-    model: "Personne",
-    data: personne
-  })
-  file.push({
-    model: "Patrimoine",
-    data: patrimoine
-  })
-  return writeFile("./fileManager/data.json", file)
+// }
+// function read() {
+//   return readFile("./fileManager/data.json")
+// }
 
-}
-function read() {
-  return readFile("./fileManager/data.json")
-}
+const fileData = fileURLToPath(import.meta.url);
+const dirname =  path.dirname(fileData);
+const folderData = path.resolve(dirname, './data/data.json');
 
-export {save, read}
+console.log("Script démarré");
+writeFile(folderData, johnPatrimoine);
+console.log("Écriture terminée");
