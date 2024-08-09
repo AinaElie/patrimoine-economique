af1f4a29f9931acff4fa32717170004ba522a4f3
AinaElie <elie.anjarafanomezana@gmail.com>
2024-08-09 15:25:48 +0300

    feat: date

--- instance.js
+++ instance.js
@@ -8,12 +8,11 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const john = new Personne("John Doe");
-const dateToday = new Date();
 
-const macBookPro = new Possession(john, "MacBook Pro", 4000000, dateToday, null, 5);
-const salaire = new Flux(john,"Alternance",500_000, dateToday,null,null,1);
-const traindevie = new Flux(john,"Survie",-300_000, dateToday,null,null,2)
-const bienMateriel = new BienMateriel(john, "Effet vestimentaire", 1_000_000, dateToday, null, 20);
+const macBookPro = new Possession(john, "MacBook Pro", 4000000, new Date("2023-12-25"), null, 5);
+const salaire = new Flux(john,"Alternance",500_000, new Date("2022-12-31"),null,null,1);
+const traindevie = new Flux(john,"Survie",-300_000, new Date("2022-12-31"),null,null,2)
+const bienMateriel = new BienMateriel(john, "Effet vestimentaire", 1_000_000, new Date("2022-12-31"), null, 20);
 
 const possessions = [macBookPro,salaire,traindevie,bienMateriel];
 
