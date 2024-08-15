cb31d0b35b3e07a6dd4de4aa6f53fcdd8f6e01c0
AinaElie <elie.anjarafanomezana@gmail.com>
2024-08-15 11:24:14 +0300

    feat: boutton create

---
+++ ui/src/Header/Possession.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ListPossession from '../Pages/ListPossession'
+import ButtonCreate from '../Components/ButtonCreate'
+
+export default function Possession() {
+  return (
+    <div>
+        <ListPossession/>
+        <ButtonCreate/>
+    </div>
+  )
+}
