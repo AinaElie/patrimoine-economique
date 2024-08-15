3a9b79fc0e72b144086119e9d7dd9f9069964d66
AinaElie <elie.anjarafanomezana@gmail.com>
2024-08-15 11:00:16 +0300

    feat: endpoint /possession

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,7 @@
-// import express from "express";
-// import cors from 'cors';
-
-// const port = 5173;
-// const app = express();
-// app.use(cors());
-
-// app.get('/possession', (req, res) => {
-//     res.writeHead("Content-Type", "application/json");
-//     res.json('../data/data.json')
-// });
-
-// app.listen(port, () => {
-//     console.log(`Serveur : http://localhost:${port}`);
-// })
-
 import express from 'express';
 import fs from 'fs/promises';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import cors from 'cors';
 
 const app = express();
@@ -24,12 +9,12 @@ const port = 5000;
 
 app.use(cors());
 
-// Fonction pour lire les données JSON
 const getDataFromJson = async () => {
-  // Chemin vers le fichier JSON dans le dossier data
-  const filePath = path.join(__dirname, '../data/data.json');
-  const fileData = await fs.readFile(filePath, 'utf8');
-  return JSON.parse(fileData);
+  const fileData = fileURLToPath(import.meta.url);
+  const dirname = path.dirname(fileData);
+  const filePath = path.join(dirname, '../data/data.json');
+  const folderData = await fs.readFile(filePath, 'utf8');
+  return JSON.parse(folderData);
 };
 
 app.get('/possession', async (req, res) => {
@@ -37,13 +22,9 @@ app.get('/possession', async (req, res) => {
     const data = await getDataFromJson();
     res.json(data);
   } catch (error) {
-    res.status(500).send('Erreur lors de la lecture des données');
+    res.status(500).send('Erreur lors de la lecture des données : ' + error);
   }
 });
-
-// app.get('/data', async (req, res) => {
-//     res.send("Hello");
-// });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
