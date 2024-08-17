403cb0bed3e44de00cf0fc0420bae6c12d7469fc
AinaElie <elie.anjarafanomezana@gmail.com>
2024-08-17 20:58:36 +0300

    feat: new target

--- ui/src/Header/Possession.jsx
+++ ui/src/Header/Possession.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ListPossession from '../Pages/ListPossession'
 import Button from '../Components/Button'
-// import ButtonCreate from '../Components/ButtonCreate'
 
 export default function Possession() {
   return (
     <div>
         <ListPossession/>
-        {/* <ButtonCreate/> */}
         <Button print={"Create Possession"} target={"create"} />
         <Button print={"Menu"} target={"/"} />
     </div>
