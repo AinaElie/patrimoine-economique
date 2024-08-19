e77094a28754ca38781573e7b57a51ba4344649d
AinaElie <elie.anjarafanomezana@gmail.com>
2024-08-19 15:00:54 +0300

    feat: style

--- ui/src/Pages/ListPossession.jsx
+++ ui/src/Pages/ListPossession.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom'
+import Possession from '../../../models/possessions/Possession.js';
 
 export default function ListPossession() {
     const [data, setData] = useState(null);
@@ -48,18 +49,21 @@ export default function ListPossession() {
                     </thead>
                     <tbody className='bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700'>
                         {data.data.possessions.map((possession, index) => (
-                            <tr key={index} className="hover:bg-gray-100">
-                                <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white" >{possession.libelle}</td>
-                                <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{Math.abs(possession.valeur) || Math.abs(possession.valeurConstante)}</td>
+                            <tr key={index} className="hover:bg-blue-100">
+                                <td className="py-4 px-6 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white" >{possession.libelle}</td>
+                                <td className="py-4 px-6 text-lg font-bold text-gray-900 whitespace-nowrap dark:text-white">{Math.abs(possession.valeur) || Math.abs(possession.valeurConstante)}</td>
                                 <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{new Date(possession.dateDebut).toLocaleDateString()}</td>
                                 <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{"..."}</td>
                                 <td className="py-4 px-8 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                     {possession.tauxAmortissement !== null ? `${possession.tauxAmortissement}%` : 0 + '%'}
                                 </td>
                                 <td scope="col" className="py-3 px-6 text-lg text-gray-700 text-left flex items-center">
-                                    <Link to={`:${possession.libelle}/update`} className='bg-blue-600 px-4 py-2 mr-2 text-white rounded-lg'>Edit</Link>
-                                    {/* <Link to=":libelle/update" className='bg-blue-600 px-4 py-2 mr-2 text-white rounded-lg'>Edit</Link> */}
-                                    <Link className='px-4 py-2 bg-red-600 text-white rounded-lg'>Close</Link>
+                                    <Link to={`:${possession.libelle}/update`} className='mx-4 text-xl'>
+                                        <i class="fa-solid fa-pen-to-square"></i>
+                                    </Link>
+                                    <Link className='text-xl'>
+                                        <i class="fa-regular fa-circle-xmark"></i>
+                                    </Link>
                                 </td>
                             </tr>
                         ))}
