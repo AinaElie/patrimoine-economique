2060776e21f2f962ea028a388f314a704ef6eaf4
AinaElie <elie.anjarafanomezana@gmail.com>
2024-08-26 16:57:54 +0300

    feat: new endpoint /patrimoine/:date

--- ui/src/Header/Patrimoine.jsx
+++ ui/src/Header/Patrimoine.jsx
@@ -1,10 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import LineChart from '../Pages/LineChart'
+import Possession from '../../../models/possessions/Possession';
+import Flux from '../../../models/possessions/Flux';
+import InstancePatrimoine from '../../../models/Patrimoine.js';
+import Personne from '../../../models/Personne.js';
+import { Link } from 'react-router-dom';
 
 export default function Patrimoine() {
+  const [data, setData] = useState(null);
+  const [value, setValue] = useState("");
+  const [date, setDate] = useState("");
+
+  function handleDate(event) {
+    event.preventDefault();
+    setValue("");
+    setDate(value);
+  }
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        let reponse = await fetch('http://localhost:5000/patrimoine', { method: 'GET' });
+
+        if (reponse.ok) {
+          const data = await reponse.json();
+          setData(data.data);
+          // console.log("Les donnes : ", data);
+        } else {
+          console.log("Erreur : ", reponse);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getData();
+  }, []);
+
+  if (!data) {
+    return <div>Aucune donnée trouvée</div>;
+  }
+
+  const LesPossessions = data.possessions.filter(element => element.valeur !== 0);
+  const newPossession = LesPossessions.map(element => new Possession(element.possesseur, element.libelle, element.valeur, new Date(element.dateDebut), element.dateFin === null ? "..." : new Date(element.dateFin), element.tauxAmortissement));
+  const LesFlux = data.possessions.filter(element => element.valeur == 0);
+  const newFlux = LesFlux.map(element => new Flux(element.possesseur, element.libelle, element.valeurConstante, new Date(element.dateDebut), element.dateFin === null ? "..." : new Date(element.dateFin), element.tauxAmortissement, element.jour));
+  const possessions = newPossession.concat(newFlux);
+
+  const personne = new Personne(data.possesseur.nom);
+  const patrimoine = new InstancePatrimoine(personne, possessions);
+
   return (
-    <div>
-      <LineChart />
-    </div>
+    <>
+      <div>
+        <LineChart />
+      </div>
+      <form onSubmit={handleDate}>
+        <h1 className='text-xl py-4'>Selectionner la date : </h1>
+        <input type="date" onChange={(event) => setValue(event.target.value)} value={value} className='border border-gray-600 py-2 px-4 rounded-lg' />
+        <button className='bg-blue-600 mx-2 py-3 px-4 rounded-xl text-white' type='submit'>
+          <Link to={`:${value}`} >Valider</Link>
+        </button>
+        <p className='text-xl py-4'>Valeur du patrimoine est :
+          <span className='font-semibold text-2xl'>
+            {patrimoine.getValeur(new Date(date)).toFixed(0)}
+          </span>
+        </p>
+      </form>
+    </>
   )
 }
