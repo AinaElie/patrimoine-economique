336d197ff8cd022c0dfaa854da0efc84b5899b6e
AinaElie <elie.anjarafanomezana@gmail.com>
2024-08-26 19:53:08 +0300

    style

--- ui/src/Header/Patrimoine.jsx
+++ ui/src/Header/Patrimoine.jsx
@@ -4,7 +4,7 @@ import Possession from '../../../models/possessions/Possession';
 import Flux from '../../../models/possessions/Flux';
 import InstancePatrimoine from '../../../models/Patrimoine.js';
 import Personne from '../../../models/Personne.js';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function Patrimoine() {
   const [data, setData] = useState(null);
@@ -27,7 +27,6 @@ export default function Patrimoine() {
         if (reponse.ok) {
           const data = await reponse.json();
           setData(data.data);
-          // console.log("Les donnes : ", data);
         } else {
           console.log("Erreur : ", reponse);
         }
@@ -60,7 +59,7 @@ export default function Patrimoine() {
         <h1 className='text-xl py-4'>Selectionner la date : </h1>
         <input type="date" onChange={(event) => setValue(event.target.value)} value={value} className='border border-gray-600 py-2 px-4 rounded-lg' />
         <button className='bg-blue-600 mx-2 py-3 px-4 rounded-xl text-white' type='submit'>Valider</button>
-        <p className='text-xl py-4'>Valeur du patrimoine est :
+        <p className='text-xl py-4'>La valeur du patrimoine est :
           <span className='font-semibold text-2xl mx-1'>
             {patrimoine.getValeur(new Date(date)).toFixed(0)}
           </span>
